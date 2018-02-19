6bbcfce96f68c0bf9df49e53ad18486f21940295
Pattaphol Jirasetsakul <pattaphol_j@hotmail.com>
2018-02-19 14:19:22 -0800

    Added XML metafiles for DBs
    Added way to create XML metadata file for each databases that are created

--- PA1/temp.py
+++ PA1/temp.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
 import cmd, sys
 import os
+import xml.etree.cElementTree as ET
 
 runProgram = True;
 
 
-#while (runProgram == True)
+
 class Interface(cmd.Cmd):
     intro = 'PA1 Assignment by Pattaphol Jirasessakul.\n'
     prompt = '(>) '
@@ -34,6 +31,8 @@ class Interface(cmd.Cmd):
         line = line.lower()
         return line
 
+    #command_list = list commands user enter before entering "go"
+    #command = one particular command from command_list
  
             
     def do_create(self,arg):
@@ -44,6 +43,14 @@ class Interface(cmd.Cmd):
          
         print(self.command_list)
       #  print(self.command_list[0][3])
+    
+    
+    def do_alter(self,arg):
+        command = arg.split(" ")  
+        command.insert(0,"alter")
+        self.command_list.append(command)
+        print(self.command_list)
+    
     
       
     def do_go(self,arg):
@@ -66,9 +73,11 @@ class Interface(cmd.Cmd):
                 
             if command[0] == "alter":
                 print("call alter")
-
-        self.command_list.clear
+        
+        print(self.command_list)
     
+        self.command_list.clear()
+        print(self.command_list)
    
     def do_use(self,arg):
         
@@ -79,11 +88,18 @@ class Interface(cmd.Cmd):
         
         if(command[1].lower() == "database"):
             print("call create database")
-            return self.createDB(command[2]);
+            return self.createDB(command[2]); #command[2] represents name of of db or table
+        
+        
         
         elif(command[1].lower() == "table"):
-            print("call create table")
-            return self.createTable(command[2],self.DBInUse)
+            if (len(command) == 2):
+                print("Can't create table without table name")
+                return False
+            else:
+                
+                return self.createTable(command[2],self.DBInUse)
+        
         
         else:
             print("Syntax error with create")
@@ -95,28 +111,57 @@ class Interface(cmd.Cmd):
         directory = self.directory + DBName
         if not os.path.exists(directory):
             os.makedirs(directory)
+            self.initializeMeta(DBName,directory)
             return True;
         else:
             print("Database " +DBName + ", already exists")
             return False;
 
+
     def createTable(self,tableName,DBInUse):
         
-        tableName = tableName+".txt"
+         
+        tableName = tableName
         tableLocation = os.path.join(self.directory,DBInUse,tableName) 
+        
         if(self.DBInUse == "None"):
             print("No Database in use, can't create a table")
             return False
+        
+        
+        os.chdir(os.path.join(self.directory,DBInUse))
+        tree= ET.parse(DBInUse+".xhtml")
+        root = tree.getroot()
+         
+         
+        if (root.find(tableName) == None):
+               # open(tableLocation, "a")
+                #print(os.path.join(self.directory,DBInUse))
+            
+            c= ET.SubElement(root,tableName)
+            c.text = 'test'
+           # print (ET.tostring(root))
+                 
+            tree.write(DBInUse+".xhtml")
+#                DB = root.find(root.tag)
+                #print(DB.tag)
+                 
+                
+            return True
         else:
-            if not os.path.exists(tableLocation):
-                open(tableLocation, "a")
-                return True
-            else:
-                print("Table already exists in current database")
-                return False    
-#
-#
-#
+            print("Table already exists in current database")
+            return False    
+
+ #########################################      
+
+    def initializeMeta(self,DBName,directory):
+        root = ET.Element(DBName)
+            
+        # wrap it in an ElementTree instance, and save as XML
+        tree = ET.ElementTree(root)
+        os.chdir(directory)
+        tree.write(DBName+".xhtml")
+
 #
 #
 #
